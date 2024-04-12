1fd7651b5f0adaa64308a38b3b470a565c8c30c1
waka-bltinc <swakatsuki@bltinc.co.jp>
2024-04-12 16:49:07 +0900

    clean: スクリプトをリファクタリング

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,39 @@
-document.addEventListener("scroll", function () {
+/**
+ * スクロール位置に基づいて要素の背景色を変更する
+ */
+const changeBackgroundColorOnScroll = () => {
     const contentElement = document.getElementById("content__white");
     const mainElement = document.getElementById("content__black");
     const mainBottom = mainElement.offsetTop + mainElement.offsetHeight;
 
     if (window.scrollY > mainBottom) {
-        // main要素を超えたスクロール量
         const scrollAmount = window.scrollY - mainBottom;
-
-        // 色の変化を制限（最大242とする）
-        let colorValue = Math.min(242, scrollAmount); // 0.1は変化速度を調整する係数
-        colorValue = Math.floor(colorValue); // 整数に丸める
-
-        // 背景色を設定
+        let colorValue = Math.min(242, scrollAmount);
+        colorValue = Math.floor(colorValue);
         contentElement.style.backgroundColor = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
     } else {
-        // main要素より上にいる場合は背景色をリセット
         contentElement.style.backgroundColor = 'black';
     }
-});
+}
 
-window.addEventListener("DOMContentLoaded", function () {
-    // すべての.section__header-container要素を選択
+/**
+ * 要素が画面に入ったタイミングでフェードインさせる
+ */
+const animateTextOnIntersection = () => {
     const headerContainers = document.querySelectorAll('.section__header-container');
-
-    // Intersection Observerのコールバック
     const observerCallback = (entries, observer) => {
         entries.forEach((entry) => {
-            // 対応する.section-header__text要素を取得
             const headerText = entry.target.querySelector('.section-header__text');
-
             if (entry.isIntersecting) {
                 headerText.classList.add('animate__animated', 'animate__fadeInLeft');
             }
         });
     };
-
-    // Intersection Observerの設定
     const observer = new IntersectionObserver(observerCallback);
-
-    // それぞれの.section__header-container要素に対してオブザーバーを設定
     headerContainers.forEach(container => {
         observer.observe(container);
     });
-});
+}
+
+document.addEventListener("scroll", changeBackgroundColorOnScroll);
+window.addEventListener("DOMContentLoaded", animateTextOnIntersection);
