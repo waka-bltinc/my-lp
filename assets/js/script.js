82ec4f592f932e64ee8c71ec61b29197d61cf697
waka-bltinc <swakatsuki@bltinc.co.jp>
2024-04-15 11:59:56 +0900

    update: detailsセクションの上余白を調整したことに伴いスクリプトを変更

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,10 @@ const changeBackgroundColorOnScroll = () => {
     const mainElement = document.getElementById("content__black");
     const mainBottom = mainElement.offsetTop + mainElement.offsetHeight;
 
-    if (window.scrollY > mainBottom) {
+    console.log(mainBottom);
+
+    // スクロール位置がmain要素の真ん中あたりに来たら背景色を変更
+    if (window.scrollY > mainBottom - window.innerHeight / 2) {
         // 時間経過で徐々にcontent__whiteのの背景色を白くする
         contentElement.style.backgroundColor = 'white';
         contentElement.style.transition = 'background-color 1s';
